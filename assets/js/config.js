a17c607b607f61909150949436f99517c52155b7
technocodech <ikrommaulana27588@gmail.com>
2026-02-17 16:11:45 +0700

    Update BASE_URL to use Ngrok URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 // ============================================
         // PRODUCTION CONFIGURATION - GANTI DENGAN NGROK URL ANDA
         // ============================================
-        const BASE_URL = "http://0.0.0.0:8000";
+        const BASE_URL = "https://c5ca-45-84-123-213.ngrok-free.app";
         window.BASE_URL = BASE_URL;
         window.APP_NAME = "SMK MBP";
         window.PPDB_YEAR = "2026/2027";
